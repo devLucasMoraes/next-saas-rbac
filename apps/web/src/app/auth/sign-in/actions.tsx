51597b35ff2001694c9b8741524af68bad0cff51
devLucasMoraes <devlucasmoraes@gmail.com>
2025-05-11 18:13:30 -0300

    feat: usando hook useActionState
    e adiciona estados de erro do formulario

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,14 +1,47 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
 import { signInWithEmailAndPassword } from '@/http/sing-in-with-email-password'
 
-export async function singInWithEmailAndPassword(data: FormData) {
-  const { email, password } = Object.fromEntries(data)
+const signInSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(6, {
+    message: 'Password must be at least 6 characters long',
+  }),
+})
 
-  const result = await signInWithEmailAndPassword({
-    email: String(email),
-    password: String(password),
-  })
+export async function singInWithEmailAndPassword(
+  previousData: unknown,
+  data: FormData,
+) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
 
-  console.log(result)
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithEmailAndPassword({
+      email,
+      password,
+    })
+
+    console.log({ token })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+    return { success: false, message: 'Unexpected error', errors: null }
+  }
+
+  return { success: true, message: null, errors: null }
 }
