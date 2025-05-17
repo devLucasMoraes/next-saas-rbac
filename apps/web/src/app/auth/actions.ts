e9f9878275b96bba408f265178c346b0e57c021b
devLucasMoraes <devlucasmoraes@gmail.com>
2025-05-17 20:58:28 -0300

    feat: utiliza variaveis de ambiente no projeto web

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,15 +1,13 @@
 'use server'
 
+import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGithub() {
   const githubUrl = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubUrl.searchParams.set('client_id', 'alndandjajdbajbda')
-  githubUrl.searchParams.set(
-    'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
-  )
+  githubUrl.searchParams.set('client_id', env.GITHUB_CLIENT_ID)
+  githubUrl.searchParams.set('redirect_uri', env.GITHUB_REDIRECT_URI)
   githubUrl.searchParams.set('scope', 'read:user')
 
   redirect(githubUrl.toString())
--- packages/env/index.ts
+++ packages/env/index.ts
@@ -12,7 +12,9 @@ export const env = createEnv({
     GITHUB_CLIENT_SECRET: z.string(),
     GITHUB_REDIRECT_URI: z.string().url(),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+  },
   shared: {},
   runtimeEnv: {
     SERVER_PORT: process.env.SERVER_PORT,
@@ -21,6 +23,7 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     GITHUB_REDIRECT_URI: process.env.GITHUB_REDIRECT_URI,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   emptyStringAsUndefined: true,
 })
